3c0f4ee85ac2bc8fcfefe80c455f9f76ff8e22de
Forrest Benton <elliottbenton@gmail.com>
2018-06-15 16:37:18 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,3 +1,3 @@
 def display_rainbow(color_array)
-  puts "R: #{color_array[0],"
+  puts "R: #{color_array[0]}, O: #{color_array[1], R: #{color_array[0], R: #{color_array[0], R: #{color_array[0], R: #{color_array[0], R: #{color_array[0],"
 end
